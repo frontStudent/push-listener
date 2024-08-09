553459a98cfee66b76db4531ece77a8848c3a585
shiyubei <shiyubei@qince.com>
2024-08-09 11:03:58 +0800

    feat: init repo

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { exec } = require("child_process");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.post("/where-we-meet", (req, res) => {
+  // 切换到root目录，执行 ./ci.sh 脚本
+  exec(
+    `cd ~/where-we-meet && chmod +x ci.sh && ./ci.sh`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行错误: ${error}`);
+        return res.status(500).send(`执行错误: ${stderr}`);
+      }
+      console.log(`stdout: ${stdout}`);
+      res.send(`执行成功: ${stdout}`);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
