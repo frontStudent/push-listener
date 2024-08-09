57b29d22e03090ed92c58dcbd82abe9b07224ab3
shiyubei <shiyubei@qince.com>
2024-08-09 13:21:17 +0800

    feat: 默认根路由

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send('Hello World!')
+});
+
 app.post("/where-we-meet", (req, res) => {
   // 切换到root目录，执行 ./ci.sh 脚本
   exec(
