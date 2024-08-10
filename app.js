7a20812dc8e806f1e19299e182663f5dcb6687c7
shiyubei <shiyubei@qince.com>
2024-08-10 12:57:15 +0800

    feat: webhook

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const { exec } = require("child_process");
-const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send('Hello World!')
+// 用于测试node服务是否正常运行
+app.get("/api/*", (req, res) => {
+  res.send(req.path.slice('/api/'.length));
 });
 
+// 快速测试webhook是否正常接收
 app.post("/webhook", (req, res) => {
   res.send("webhook test");
 });
 
-app.post("/where-we-meet", (req, res) => {
+// 接收webhook请求，并执行脚本
+app.post("/webhook-api/*", (req, res) => {
+  const PROJECT_NAME = req.path.slice("/webhook-api/".length);
   // 切换到root目录，执行 ./ci.sh 脚本
   exec(
-    `cd ~/where-we-meet && chmod +x ci.sh && ./ci.sh`,
+    `cd ~/${PROJECT_NAME} && chmod +x ci.sh && ./ci.sh`,
     (error, stdout, stderr) => {
       if (error) {
         console.error(`执行错误: ${error}`);
         return res.status(500).send(`执行错误: ${stderr}`);
       }
       console.log(`stdout: ${stdout}`);
-      res.send(`执行成功: ${stdout}`);
     }
   );
+  res.send(`deploying...`);
 });
 
 app.listen(port, () => {
